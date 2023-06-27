74b8f2315888e5f73c071c4e4512482c9bd12f35
Erick Hernández <erickhernandez0492@gmail.com>
2023-06-27 11:31:11 +0200

    Registrando componente ItemsFactura

--- src/main/java/com/bolsadeideas/springboot/di/app/AppConfig.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/AppConfig.java
@@ -1,11 +1,16 @@
 package com.bolsadeideas.springboot.di.app;
 
+import com.bolsadeideas.springboot.di.app.models.domain.ItemFactura;
+import com.bolsadeideas.springboot.di.app.models.domain.Producto;
 import com.bolsadeideas.springboot.di.app.models.service.IServicio;
 import com.bolsadeideas.springboot.di.app.models.service.MiServicio;
 import com.bolsadeideas.springboot.di.app.models.service.MiServicioComplejo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class AppConfig {
@@ -21,4 +26,17 @@ public class AppConfig {
     public IServicio registrarMiServicioComplejo(){
         return new MiServicioComplejo();
     }
+
+    @Bean("itemsFactura")
+    public List<ItemFactura> registrarItems(){
+        Producto producto1 = new Producto("Camara Sony", 100);
+        Producto producto2 = new Producto("Bicicleta Bianchi aro 26", 200);
+
+        ItemFactura linea1 = new ItemFactura(producto1, 2);
+        ItemFactura linea2 = new ItemFactura(producto2, 2);
+
+        return Arrays.asList(linea1, linea2);
+
+    }
+
 }
--- src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
+++ src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
@@ -9,11 +9,12 @@ import java.util.List;
 @Component
 public class Factura {
 
-    @Value("${descripcion.factura}")
+    @Value("${factura.descripcion}")
     private String descripcion;
 
     @Autowired
     private Cliente cliente;
+    @Autowired
     private List<ItemFactura> item;
 
     public String getDescripcion() {
